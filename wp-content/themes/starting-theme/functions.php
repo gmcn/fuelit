c78af606e36b5ce8c39c47b2ae175f688dcac620
Web Dev <webdev@Webs-MBP.lan>
2019-01-14 17:28:16 +0000

    add products custom post and product area taxonomy

--- wp-content/themes/starting-theme/functions.php
+++ wp-content/themes/starting-theme/functions.php
@@ -263,7 +263,7 @@ function case_studies_post_type() {
 		'description'           => __( 'Case Study information page.', 'text_domain' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions', 'featured' ),
-		'taxonomies'            => array( 'category' ),
+		// 'taxonomies'            => array( 'category' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
@@ -348,6 +348,118 @@ function case_studies_taxonomies()
   register_taxonomy('tag','case_studies',array(
     'hierarchical' => false,
     'labels' => $casestudy_tags_args,
+    'show_ui' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'tag' ),
+  ));
+}
+
+// Register Custom Post Type
+function products_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Products', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Product', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Products', 'text_domain' ),
+		'name_admin_bar'        => __( 'Product', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Product', 'text_domain' ),
+		'description'           => __( 'Product information page.', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions', 'featured' ),
+		// 'taxonomies'            => array( 'category' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-clipboard',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'products', $args );
+
+}
+add_action( 'init', 'products_post_type', 0 );
+
+add_action( 'init', 'products_taxonomies', 0 );
+
+//create two taxonomies, genres and tags for the post type "tag"
+function products_taxonomies()
+{
+	// product taxonomy
+    $products_area_labels = array(
+        'name'          => 'Product Area',
+        'singular_name' => 'Product Area',
+        'menu_name'     => 'Product Area'
+    );
+
+    $products_area_args = array(
+        'labels'                     => $products_area_labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+        'rewrite'                    => array('pages' => true)
+    );
+    register_taxonomy( 'products_area', array( 'products' ), $products_area_args );
+
+
+
+  // Add new taxonomy, NOT hierarchical (like tags)
+  $products_tags_args = array(
+    'name' => _x( 'Tags', 'Product' ),
+    'singular_name' => _x( 'Tag', 'Products' ),
+    'search_items' =>  __( 'Search Tags' ),
+    'popular_items' => __( 'Popular Tags' ),
+    'all_items' => __( 'All Tags' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Tag' ),
+    'update_item' => __( 'Update Tag' ),
+    'add_new_item' => __( 'Add New Tag' ),
+    'new_item_name' => __( 'New Tag Name' ),
+    'separate_items_with_commas' => __( 'Separate tags with commas' ),
+    'add_or_remove_items' => __( 'Add or remove tags' ),
+    'choose_from_most_used' => __( 'Choose from the most used tags' ),
+    'menu_name' => __( 'Product Tags' ),
+  );
+
+  register_taxonomy('tag','products',array(
+    'hierarchical' => false,
+    'labels' => $products_tags_args,
     'show_ui' => true,
     'update_count_callback' => '_update_post_term_count',
     'query_var' => true,
