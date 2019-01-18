74b8c1c9873033e0c991b2caeeffba0c39f30e35
Web Dev <webdev@Webs-MBP.lan>
2019-01-18 16:02:31 +0000

    fixed call-back element and related case studies w/industries

--- wp-content/themes/starting-theme/inc/page-elements/dynamic-industries.php
+++ wp-content/themes/starting-theme/inc/page-elements/dynamic-industries.php
@@ -5,49 +5,53 @@ $page_tags = get_the_tags();
 
 ?>
 
-<div class="row no-gutters dynamic clear">
+<?php if($page_tags) : ?>
 
-  <div class="col-xs-12 industry casestudy_posts">
+  <div class="row no-gutters dynamic clear">
 
-    <div class="col-md-12">
-      <p>Related Case Studies - <a href="/case_studies/">View all</a></p>
-    </div>
+    <div class="col-xs-12 industry casestudy_posts">
 
-    <?php foreach( $page_tags as $page_tag ) : ?>
-    <?php
+      <div class="col-md-12">
+        <p>Related Case Studies - <a href="/case_studies/">View all</a></p>
+      </div>
 
-    $args = array(
-      'post_type' => 'case_studies',
-      'tax_query' => array(
-           array(
-               'taxonomy' => 'case_studies_tag',
-               'posts_per_page' => 4,
-               'field' => 'slug',
-               'terms' => $page_tag->slug,
-           ),
-       ),
-    );
-    $query = new WP_Query( $args ); ?>
+      <?php foreach( $page_tags as $page_tag ) : ?>
+      <?php
 
-    <?php if ( $query->have_posts() ) : ?>
+      $args = array(
+        'post_type' => 'case_studies',
+        'tax_query' => array(
+             array(
+                 'taxonomy' => 'case_studies_tag',
+                 'posts_per_page' => 4,
+                 'field' => 'slug',
+                 'terms' => $page_tag->slug,
+             ),
+         ),
+      );
+      $query = new WP_Query( $args ); ?>
 
-     <!-- the loop -->
-     <?php while ( $query->have_posts() ) : $query->the_post();
+      <?php if ( $query->have_posts() ) : ?>
 
-     $title = get_the_title();
+       <!-- the loop -->
+       <?php while ( $query->have_posts() ) : $query->the_post();
 
-     ?>
-       <div class="col-md-3 post" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>) center center; background-size: cover;">
-         <div class="post__wrapper">
-           <h4><?php echo wp_trim_words($title, 8); ?></h4>
-           <a href="<?php echo the_permalink(); ?>">View Case Study</a>
+       $title = get_the_title();
+
+       ?>
+         <div class="col-md-3 post" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>) center center; background-size: cover;">
+           <div class="post__wrapper">
+             <h4><?php echo wp_trim_words($title, 8); ?></h4>
+             <a href="<?php echo the_permalink(); ?>">View Case Study</a>
+           </div>
          </div>
-       </div>
-     <?php endwhile; endif; wp_reset_postdata();  ?>
+       <?php endwhile; endif; wp_reset_postdata();  ?>
 
 
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+
+    </div>
 
   </div>
 
-</div>
+<?php endif; ?>
