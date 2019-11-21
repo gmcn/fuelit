f1d3e06a03ebe6747b5784e3224142176aa37b55
Web Dev <webdev@Webs-MBP.lan>
2019-11-21 11:51:40 +0000

    fixed page title issue

--- wp-content/themes/starting-theme/inc/page-elements/title.php
+++ wp-content/themes/starting-theme/inc/page-elements/title.php
@@ -2,6 +2,13 @@
   <div class="row">
     <div class="col-md-6">
 
+      <?php if (is_tax()): ?>
+        <h1><?php
+        $taxonomy = get_queried_object();
+        echo  $taxonomy->name; ?>
+        </h1>
+      <?php endif; ?>
+
       <?php if (is_single() && 'post' == get_post_type()) : ?>
 
         <h1>News & Events</h1>
@@ -10,6 +17,7 @@
         <h1><?php echo get_the_title( wp_get_post_parent_id( get_the_ID() ) ); ?> </h1>
       <?php elseif ( have_posts() ) : ?>
         <h1><?php single_post_title(); ?><?php post_type_archive_title(); ?></h1>
+
       <?php else : ?>
         <h1><?php the_title(); ?></h1>
       <?php endif; ?>
@@ -27,9 +35,11 @@
   <div class="row">
     <div class="col-md-6 title__content">
 
-
-
-      <?php if ( $post->post_parent == 142 || $post->post_parent == 398) : ?>
+      <?php if (is_tax()): ?>
+        <?php
+        // $taxonomy = get_queried_object();
+        echo  $taxonomy->description; ?>
+      <?php elseif ( $post->post_parent == 142 || $post->post_parent == 398) : ?>
         <?php echo the_field('sub_heading_text', $post->post_parent); ?>
       <?php else : ?>
         <?php //the_content(); ?>
